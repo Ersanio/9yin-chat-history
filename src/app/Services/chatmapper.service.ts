d1e212e46256bf31189bfa2b539db2239d403239
Ersanio <contact@ersan.io>
2021-03-24 23:32:33 +0100

    Add chat list name sorting. Closes #2

--- src/app/Services/chatmapper.service.ts
+++ src/app/Services/chatmapper.service.ts
@@ -20,6 +20,13 @@ export class ChatmapperService {
     } else {
       history.chatRecords.push(this.parseMessagesFromRecord(xmlInput.Records.Record));
     }
+
+    history.chatRecords = history.chatRecords.sort((x, y) => {
+      const a = x.chatName.toLowerCase();
+      const b = y.chatName.toLowerCase();
+      return (a < b) ? -1 : (a > b) ? 1 : 0;
+    });
+
     return history;
   }
 
@@ -45,9 +52,9 @@ export class ChatmapperService {
     content = content.replace(new RegExp(/<font .*>(.*)<\/font>/g), '$1'); // completely remove font tags
     content = content.replace(new RegExp(/<br\/>/g), ''); // completely remove br tags
     content = content
-                .replace(new RegExp(/<img src="([a-zA-Z0-9_]*)".*/g), '<img src="assets/emotes/$1.png" />') // emotes with image tags
-                .replaceAll('_', ''); // quick fix to remove underscore from the file path, Angular can't handle space and underscores in asset path
-                                      // todo: unsatisfying fix, possible impact on performance. Is there a way to support underscores in assets?
+      .replace(new RegExp(/<img src="([a-zA-Z0-9_]*)".*/g), '<img src="assets/emotes/$1.png" />') // emotes with image tags
+      .replaceAll('_', ''); // quick fix to remove underscore from the file path, Angular can't handle space and underscores in asset path
+    // todo: unsatisfying fix, possible impact on performance. Is there a way to support underscores in assets?
 
     content = content.replace(new RegExp(/<a .*><(.*)><\/a>/g), '<span class="item">&lt;$1&gt;</span>'); // replace item links
     return content;
